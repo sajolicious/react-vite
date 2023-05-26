765fb63b5d058d1b92b55ea0141be6f75ecaa250
Sajol Das <sajoldas421@example.com>
2023-05-27 04:58:27 +0600

    first

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './components/header/header'
+import './App.css'
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  )
+}
+
+export default App;
